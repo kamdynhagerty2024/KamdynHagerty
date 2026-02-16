a146c2fca6ab35f5e46747d29b81987f757afb26
kamdynhagerty2024 <kamdynhagerty2024@gmail.com>
2026-02-16 12:35:11 -0700

    Add files via upload

---
+++ Kamdyn week 4.py
@@ -0,0 +1,22 @@
+#Name: Section3Problem11
+#Purpose: Turtle
+#Author: KamdynHagerty
+#Created: 2/16/26
+
+import turtle
+
+
+wn = turtle.Screen()
+tess = turtle.Turtle()
+tess.hideturtle()      
+tess.speed(5)          
+tess.pendown()
+for i in range(5):
+    tess.forward(150)   
+    tess.right(144)     
+   
+
+
+
+
+
